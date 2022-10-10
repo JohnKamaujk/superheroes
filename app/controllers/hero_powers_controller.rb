717d8046d6f942ecd257aa7d8027123c4f88c738
John Kamau <johnjkkamau@gmail.com>
2022-10-10 15:32:20 +0300

    Done

---
+++ app/controllers/hero_powers_controller.rb
@@ -0,0 +1,19 @@
+class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        hero_power = HeroPower.create!(hero_power_params)
+        hero = Hero.find(hero_power.hero_id)
+        render json: hero, status: :created
+    end
+
+    private 
+
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,16 +1,39 @@
 class PowersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index 
         powers = Power.all
         render json: powers, status: :ok
     end
 
+    def show
+        power = find_power
+        render json: power, status: :ok
+    end
+
+    def update
+        power = find_power
+        power.update!(power_params)
+        render json: power
+    end
+
 
     private
+
+    def power_params
+        params.permit(:description)
+    end
+
+    def find_power
+        Power.find(params[:id])
+    end
 
     def render_not_found_response
         render json: { error: "Power not found"}, status: :not_found
     end
 
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
